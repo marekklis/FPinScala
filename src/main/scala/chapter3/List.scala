2463a6cabef02c0d1b042149302206b9f7f791a1
Marek Kliś <marek.klis@contractors.roche.com>
2014-09-30 13:36:26 +0200

    exercise 3.10 foldLeft implemented

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -86,7 +86,11 @@ object List {
 
   def length[A](l: List[A]): Int = foldRight(l, 0)((_, acc) => acc + 1)
 
-  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  @annotation.tailrec
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h, t) => foldLeft(t, f(z, h))(f)
+  }
 
   def map[A, B](l: List[A])(f: A => B): List[B] = sys.error("todo")
 }
