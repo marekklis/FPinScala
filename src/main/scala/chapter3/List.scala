807d8e9adbae6801a616ee59080fdcf4407a188b
Marek Kliś <marek.klis@contractors.roche.com>
2014-09-30 10:42:19 +0200

    exercise 3.3 setHead implemented

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -60,7 +60,10 @@ object List {
     case Cons(_, xs) => xs
   }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => sys.error("setHead of empty list")
+    case Cons(_, xs) => Cons(h, xs)
+  }
 
   def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
 
--- src/test/scala/chapter3/ListTest.scala
+++ src/test/scala/chapter3/ListTest.scala
@@ -1,22 +1,36 @@
 package chapter3
 
-import chapter3.List.tail
-import org.scalatest.FlatSpec
+import chapter3.List._
+import org.scalatest.FunSpec
 
-class ListTest extends FlatSpec {
+class ListTest extends FunSpec {
 
-  "tail of Nil" should "rise error" in {
-    intercept[RuntimeException] {
-      tail(Nil)
+  describe("tail") {
+    it("should rise error for empty list") {
+      intercept[RuntimeException] {
+        tail(Nil)
+      }
+    }
+
+    it("should return Nil for one element List") {
+      assert(tail(List(1)) == Nil)
+    }
+
+    it("should return (2,3) for (1,2,3)") {
+      assert(tail(List(1, 2, 3)) == List(2, 3))
     }
   }
 
-  "tail of (1)" should "be Nil" in {
-    assert(tail(List(1)) == Nil)
-  }
+  describe("setHead") {
+    it("should be (7,2,3) for setHead((1,2,3),7)") {
+      assert(setHead(List(1, 2, 3), 7) == List(7, 2, 3))
+    }
 
-  "tail of (1,2,3)" should "be (2,3)" in {
-    assert(tail(List(1, 2, 3)) == List(2, 3))
+    it("should rise exception for setHead(Nil,7)") {
+      intercept[RuntimeException] {
+        setHead(Nil, 7)
+      }
+    }
   }
 
 }
