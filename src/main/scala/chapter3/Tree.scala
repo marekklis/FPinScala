53e5068082daec6d416c3c71e9a9a4632c62d859
Marek Kliś <marek.klis@contractors.roche.com>
2014-10-01 15:09:28 +0200

    exercise 3.26 maximum implemented

--- src/main/scala/chapter3/Tree.scala
+++ src/main/scala/chapter3/Tree.scala
@@ -14,5 +14,9 @@ object Tree {
     case Branch(l, r) => size(l) + size(r) + 1
   }
 
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
 
 }
--- src/test/scala/chapter3/TreeTest.scala
+++ src/test/scala/chapter3/TreeTest.scala
@@ -1,6 +1,6 @@
 package chapter3
 
-import chapter3.Tree.size
+import chapter3.Tree._
 import org.scalatest.FunSpec
 
 class TreeTest extends FunSpec {
@@ -8,6 +8,12 @@ class TreeTest extends FunSpec {
   describe("size") {
     it("should be 7") {
       assert(size(Branch(Branch(Leaf("a"), Leaf("b")), Branch(Leaf("c"), Leaf("d")))) == 7)
+    }
+  }
+
+  describe("maximum") {
+    it("should be 7") {
+      assert(maximum(Branch(Branch(Leaf(1), Leaf(7)), Branch(Leaf(6), Leaf(5)))) == 7)
     }
   }
 
