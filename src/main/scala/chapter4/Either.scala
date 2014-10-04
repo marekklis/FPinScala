596061bd747b1d2a49282c5e7ed920dcaedddaf6
Marek Kliś <marek.klis@contractors.roche.com>
2014-10-04 16:01:12 +0200

    exercise 4.6 map2 implemented

--- src/main/scala/chapter4/Either.scala
+++ src/main/scala/chapter4/Either.scala
@@ -15,9 +15,13 @@ sealed trait Either[+E, +A] {
     case Right(a) => f(a)
   }
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = sys.error("todo")
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = sys.error("todo")
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {a <- this; b1 <- b} yield f(a, b1)
 }
 
 case class Left[+E](get: E) extends Either[E, Nothing]
